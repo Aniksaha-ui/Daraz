c0e8c14788126399ffaa17f694d363de26cac02a
Anik <sahaanik1045@gmail.com>
2025-02-07 13:44:08 +0600

    product

--- resources/views/pages/payment.blade.php
+++ resources/views/pages/payment.blade.php
@@ -1,207 +1,149 @@
 @extends('welcome')
 
 @section('content')
+    @php
+        $setting = DB::table('settings')->first();
+        $charge = $setting->shipping_charge;
+        $vat = $setting->vat;
+        $user = DB::table('users')->where('id', Auth::id())->first();
 
+    @endphp
 
-@php
-$setting = DB::table('settings')->first();
-$charge = $setting->shipping_charge; 
-$vat = $setting->vat; 
-@endphp
-       
-<link rel="stylesheet" type="text/css" href="{{asset('fontend/styles/contact_styles.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('fontend/styles/contact_responsive.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('fontend/styles/contact_styles.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('fontend/styles/contact_responsive.css') }}">
 
-<div class="contact_form">
+    <div class="contact_form">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-7" style="border: 1px solid grey; padding: 20px; border-radius: 25px;">
-                    <div class="contact_form_container">
-                        <div class="contact_form_title text-center">Cart Products</div>
+            <div class="row g-4">
+                <!-- Cart Products Section -->
+                <div class="col-lg-7 mx-auto">
+                    <div class="border p-4 rounded shadow-sm bg-white">
+                        <h4 class="text-center text-primary mb-4">Billing Preview</h4>
 
-          
-                          <div class="cart_items">
-                            <ul class="cart_list">
-                              
-                              @foreach($cart as $row)
-
-        <li class="cart_item clearfix">
-             
-
-
-            <div class="cart_item_info d-flex flex-md-row flex-column justify-content-between">
-
-                 <div class="cart_item_name cart_info_col">
-                    <div class="cart_item_title"><b>Product Image</b></div>
-                    <div class="cart_item_text"><img src="{{ asset($row->options->image) }} " style="width: 70px; width: 70px;" alt=""></div>
-                </div>
-
-
-                <div class="cart_item_name cart_info_col">
-                    <div class="cart_item_title"><b>Name</b></div>
-                    <div class="cart_item_text">{{ $row->name  }}</div>
-                </div>
-
-                @if($row->options->color == NULL)
-
-                @else
-                <div class="cart_item_color cart_info_col">
-                    <div class="cart_item_title"><b>Color</b></div>
-                    <div class="cart_item_text"> {{ $row->options->color }}</div>
-                </div>
-                 @endif
- 
-
-                @if($row->options->size == NULL)
-
-                @else
-                <div class="cart_item_color cart_info_col">
-                    <div class="cart_item_title"><b>Size</b></div>
-                    <div class="cart_item_text"> {{ $row->options->size }}</div>
-                </div>
-                @endif
-                  
-
-                <div class="cart_item_quantity cart_info_col">
-                    <div class="cart_item_title"><b>Quantity</b></div> 
-                 <div class="cart_item_text"> {{ $row->qty }}</div>
-           
-                </div>
-
-
-
-                <div class="cart_item_price cart_info_col">
-                    <div class="cart_item_title"><b>Price</b></div>
-                    <div class="cart_item_text">${{ $row->price }}</div>
-                </div>
-                <div class="cart_item_total cart_info_col">
-                    <div class="cart_item_title"><b>Total</b></div>
-                    <div class="cart_item_text">${{ $row->price*$row->qty }}</div>
-                </div>
-
-                 
-            </div>
-        </li>
-                                @endforeach
-                            </ul>
+                        <div class="d-flex justify-content-between mb-3">
+                            <div>
+                                <h6>Customer Information</h6>
+                                <p>Name: {{ $user->name }}</p>
+                                <p>Email: {{ $user->email }}</p>
+                                <p>Phone: {{ $user->phone ?? '' }}</p>
+                            </div>
+                            <div class="text-end">
+                                <h6>Invoice No: #INV12345</h6>
+                                <p>Date: {{ date('d M, Y') }}</p>
+                            </div>
                         </div>
 
-        <ul class="list-group col-lg-8" style="float: right;">
-            @if(Session::has('coupon'))
-            <li class="list-group-item">Subtotal : <span style="float: right;">
-            ${{ Session::get('coupon')['balance'] }} </span> </li>
-             <li class="list-group-item">Coupon : ({{ Session::get('coupon')['name'] }} )
-              <a href="{{ route('coupon.remove') }}" class="btn btn-danger btn-sm">X</a>
-           <span style="float: right;">${{ Session::get('coupon')['discount'] }} </span> </li>
-            @else
-            <li class="list-group-item">Subtotal : <span style="float: right;">
-            ${{  Cart::Subtotal() }} </span> </li>
-            @endif
-            
-          
+                        <table class="table table-bordered">
+                            <thead class="table-dark text-center">
+                                <tr>
+                                    <th>Image</th>
+                                    <th>Product</th>
+                                    <th>Color</th>
+                                    <th>Size</th>
+                                    <th>Qty</th>
+                                    <th>Unit Price</th>
+                                    <th>Total</th>
+                                </tr>
+                            </thead>
+                            <tbody class="text-center">
+                                @foreach ($cart as $row)
+                                    <tr>
+                                        <td><img src="{{ asset($row->options->image) }}" class="img-fluid rounded"
+                                                style="max-width: 50px;" alt=""></td>
+                                        <td>{{ $row->name }}</td>
+                                        <td>{{ $row->options->color ?? 'N/A' }}</td>
+                                        <td>{{ $row->options->size ?? 'N/A' }}</td>
+                                        <td>{{ $row->qty }}</td>
+                                        <td>${{ $row->price }}</td>
+                                        <td><strong>${{ $row->price * $row->qty }}</strong></td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
 
-            <li class="list-group-item">Shiping Charge : <span style="float: right;">${{ $charge  }} </span> </li>
-            <li class="list-group-item">Vat : <span style="float: right;">${{ $vat }} </span> </li>
-            @if(Session::has('coupon'))
-            <li class="list-group-item">Total : <span style="float: right;">${{ Session::get('coupon')['balance'] + $charge + $vat }} </span> </li>
-            @else
-      <li class="list-group-item">Total : <span style="float: right;">${{ Cart::Subtotal() + $charge + $vat }} </span> </li>
-            @endif
-           
-          </ul> 
-
-           
-
+                        <div class="d-flex justify-content-end">
+                            <ul class="list-group w-50">
+                                @if (Session::has('coupon'))
+                                    <li class="list-group-item">Subtotal: <span
+                                            class="float-end">${{ Session::get('coupon')['balance'] }}</span></li>
+                                    <li class="list-group-item">Coupon ({{ Session::get('coupon')['name'] }}): <span
+                                            class="float-end">-${{ Session::get('coupon')['discount'] }}</span></li>
+                                @else
+                                    <li class="list-group-item">Subtotal: <span
+                                            class="float-end">${{ Cart::Subtotal() }}</span></li>
+                                @endif
+                                <li class="list-group-item">Shipping Charge: <span
+                                        class="float-end">${{ $charge }}</span></li>
+                                <li class="list-group-item">VAT: <span class="float-end">${{ $vat }}</span></li>
+                                <li class="list-group-item fw-bold">Total: <span class="float-end">
+                                        @if (Session::has('coupon'))
+                                            ${{ Session::get('coupon')['balance'] + $charge + $vat }}
+                                        @else
+                                            ${{ Cart::Subtotal() + $charge + $vat }}
+                                        @endif
+                                    </span></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
 
- 
- 
 
-
-<div class="col-lg-5" style="border: 1px solid grey; padding: 20px; border-radius: 25px;"> 
-                    <div class="contact_form_container">
-                        <div class="contact_form_title text-center">Shipping Address</div>
-
-         <form action="{{ route('payment.process') }}" id="contact_form" method="post">
-             @csrf
-                            
-          <div class="form-group">
-    <label for="exampleInputEmail1">Full Name</label>
-    <input type="text" class="form-control"  aria-describedby="emailHelp" placeholder="Enter Your Full Name " name="name" required="">
-         </div>
-
-
-         <div class="form-group">
-    <label for="exampleInputEmail1">Phone</label>
-    <input type="text" class="form-control"  aria-describedby="emailHelp" placeholder="Enter Your Phone " name="phone" required="">
-         </div>
-
-
-         <div class="form-group">
-    <label for="exampleInputEmail1">Email</label>
-    <input type="email" class="form-control"  aria-describedby="emailHelp" placeholder="Enter Your Email " name="email" required="">
-         </div>
-
-
-         <div class="form-group">
-    <label for="exampleInputEmail1">Address</label>
-    <input type="text" class="form-control"  aria-describedby="emailHelp" placeholder="Enter Your Address" name="address" required="">
-         </div>
-
-
-
-         <div class="form-group">
-    <label for="exampleInputEmail1">City</label>
-    <input type="text" class="form-control"  aria-describedby="emailHelp" placeholder="Enter Your City" name="city" required="">
-         </div>
-
-    <div class="contact_form_title text-center"> Payment By </div>
-    <div class="form-group">
-        <ul class="logos_list">
-         
-
-              <li><input type="radio" name="payment" value="stripe"><img src="{{asset('fontend/images/stripe.png')}}" style="width: 100px;height: 60px;"></li>
-          
-              <li><input type="radio" name="payment" value="paypal"><img src="{{asset('fontend/images/stripe.png')}}" style="width: 100px;height: 60px;"></li>
-
-                 <li><input type="radio" name="payment" value="cashondelivery"><img src="{{asset('fontend/images/cashondelivery.png')}}" style="width: 100px;height: 60px;"></li>
-             
-            
-        </ul>
-        
-    </div>
- <br>
-  
-                            <div class="contact_form_button text-center">
-        <button type="submit" class="btn btn-info">Pay Now</button>
-      
+                <!-- Shipping Address Section -->
+                <div class="col-lg-5">
+                    <div class="border p-4 rounded shadow-sm bg-white">
+                        <h4 class="text-center mb-4">Shipping Address</h4>
+                        <form action="{{ route('payment.process') }}" method="post">
+                            @csrf
+                            <div class="mb-3">
+                                <label class="form-label">Full Name</label>
+                                <input type="text" class="form-control" name="name" placeholder="Enter Your Full Name"
+                                    required>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Phone</label>
+                                <input type="text" class="form-control" name="phone" placeholder="Enter Your Phone"
+                                    required>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Email</label>
+                                <input type="email" class="form-control" name="email" placeholder="Enter Your Email"
+                                    required>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Address</label>
+                                <input type="text" class="form-control" name="address" placeholder="Enter Your Address"
+                                    required>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">City</label>
+                                <input type="text" class="form-control" name="city" placeholder="Enter Your City"
+                                    required>
+                            </div>
+                            <h5 class="text-center mb-3">Payment Method</h5>
+                            <div class="d-flex justify-content-around">
+                                <label class="form-check-label">
+                                    <input type="radio" name="payment" value="stripe">
+                                    <img src="{{ asset('fontend/images/stripe.png') }}" class="img-fluid"
+                                        style="max-width: 80px;">
+                                </label>
+                                <label class="form-check-label">
+                                    <input type="radio" name="payment" value="paypal">
+                                    <img src="{{ asset('fontend/images/paypal.png') }}" class="img-fluid"
+                                        style="max-width: 80px;">
+                                </label>
+                                <label class="form-check-label">
+                                    <input type="radio" name="payment" value="cashondelivery">
+                                    <img src="{{ asset('fontend/images/cashondelivery.png') }}" class="img-fluid"
+                                        style="max-width: 80px;">
+                                </label>
+                            </div>
+                            <div class="text-center mt-4">
+                                <button type="submit" class="btn btn-info w-100">Pay Now</button>
                             </div>
                         </form>
-
                     </div>
                 </div>
-
-
-
-
-
-
-
             </div>
         </div>
-        <div class="panel"></div>
     </div>
-
-
-
-
-
-
-
-
-
-
-
-
 @endsection
